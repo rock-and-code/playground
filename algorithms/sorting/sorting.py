f2a0b67d7942b1d5176f09f3346c2f5c92d0f012
rock-and-code <105887521+rock-and-code@users.noreply.github.com>
2023-08-16 01:06:37 -0400

    sorting.py file modified and sortingTest.py file added

---
+++ algorithms/sorting/sorting.py
@@ -0,0 +1,122 @@
+from typing import TypeVar, Generic
+
+T = TypeVar("T")
+
+
+class CollectionsSorter(Generic[T]):
+    """
+    A custom helper class to sort collections using a variety
+    """
+    def __init__(self) -> None:
+        super().__init__()
+
+    # Best -> Time: O(N) [Partially sorted list] Space: O(2), average -> O(N^2) Space: O(2)
+    def insertion_sort(self, list: list[T]) -> None:
+        """
+        A custom implementaiton of the insertion sort algorithm
+        """
+        j: int = 0
+        for i in range(len(list)):
+            key = list[i]
+            j = i - 1
+            while j >= 0 and list[j] > key:
+                list[j+1] = list[j]
+                j -= 1
+            list[j+1] = key
+
+    # Best -> Time: O(LOG N) Space: O(1), Worst case -> O(N^2) Space: O(1)
+    def quick_sort(self, list: list[T]) -> None:
+        self.quick_sorter(list, 0, len(list) - 1)
+
+    def quick_sorter(self, list: list[T], left: int, right: int) -> None:
+        index: int = self.partition(list, left, right)
+        if left < index - 1:
+            self.quick_sorter(list, left, index - 1)
+        if index < right:
+            self.quick_sorter(list, index, right)
+
+    def partition(self, list: list[T], left: int, right: int) -> int:
+        pivot: int = list[(left + right) // 2]
+        while left <= right:
+            # Finding the value that does not belong on the left-hand side of the list
+            while list[left] < pivot:
+                left += 1
+            # Finding the value that does not belong on the right-hand side of the list
+            while list[right] > pivot:
+                right -= 1
+            if left <= right:
+                # Swaps values
+                temp: T = list[left]
+                list[left] = list[right]
+                list[right] = temp
+                left += 1
+                right -= 1
+
+        return left
+    
+    # Best -> Time: O(LOG N) Space: O(N), Worst case -> O(N^2) Space: O(n)
+    def merge_sort(self, list: list[T]) -> None:
+        helper: list[T] = [] * len(list)
+        self.merge_sorter(list, helper, 0, len(list) - 1)
+
+    def merge_sorter(self, list: list[T], helper: list[T], left: int, right: int) -> None:
+        if left < right:
+            middle: int = (left + right) // 2
+            self.merge_sorter(list, helper, left, middle)
+            self.merge_sorter(list, helper, middle + 1, right)
+            self.merge(list, helper, left, right, middle)
+
+    def merge(self, list: list[T], helper: list[T], left: int, right: int, middle: int) -> None:
+        # Populating helper with list's elements
+        helper = [element for element in list]
+        # defining pointers to list and helper list
+        current_index: int = left
+        helper_left: int = left
+        helper_right: int = middle + 1
+
+        while helper_left <= middle and helper_right <= right:
+            if helper[helper_left] < helper[helper_right]:
+                list[current_index] = helper[helper_left]
+                helper_left += 1
+            else:
+                list[current_index] = helper[helper_right]
+                helper_right += 1
+            current_index += 1
+        # Inserting the remaining values at the left-hand side since the right-hand side values were
+        # already in the list
+        remaining: int = middle - helper_left
+
+        for i in range(remaining+1):
+            list[current_index + i] = helper[helper_left + i]
+
+    # Worst case -> Time: O(A + B) Space: O(A + B) 
+    def counting_sorting(self, list: list[int]) -> None:
+        """
+        A custom implementation of counting sort. 
+        This sort algorithms works only for integers
+        """
+        # identifying the max value to initializes a helper arr with max_value length + 1
+        max_value: int = list[0] 
+
+        for i in range(len(list)):
+            max_value = max(max_value, list[i])
+
+        helper: list[int] = [0] * (max_value + 1)
+
+
+        for i in range(len(list)):
+            helper[list[i]] += 1
+
+        index: int = 0
+
+        for i in range(len(helper)):
+            while helper[i] > 0:
+                list[index] = i
+                index += 1
+                helper[i] -= 1
+    
+                
+
+        
+
+
---
+++ algorithms/sorting/sortingTest.py
@@ -0,0 +1,85 @@
+from sorting import CollectionsSorter
+import sys
+from timeit import default_timer as timer
+import random
+
+sorter: CollectionsSorter = CollectionsSorter()
+
+def rand_int_list_generator(max_value: int, size: int) -> list[int]:
+    helper: list[int] = []
+    for i in range(size + 1):
+        rand_int: int = random.randint(0, max_value)
+        helper.append(rand_int)
+    return helper
+
+
+
+print("#" * 5 + " Testing Insertion Sort Method " + "#" * 5)
+
+arr: list[int] = [1, 2, 4, 3, 5, 7, 9, 8, 11, 10]
+
+print(f"List before sorting it -> {arr}")
+
+sorter.insertion_sort(arr)
+
+print(f"List after sorting it -> {arr}")
+
+print("#" * 5 + " Testing Quick Sort Method " + "#" * 5)
+
+arr: list[int] = [10, 7, 1, 8, 2, 5, 3, 11, 4, 0]
+
+print(f"List before sorting it -> {arr}")
+
+sorter.quick_sort(arr)
+
+print(f"List after sorting it -> {arr}")
+
+print("#" * 5 + " Testing Merge Sort Method " + "#" * 5)
+
+arr: list[int] = [10, 7, 1, 8, 2, 5, 3, 11, 4, 0]
+
+print(f"List before sorting it -> {arr}")
+
+sorter.merge_sort(arr)
+
+print(f"List after sorting it -> {arr}")
+
+print("#" * 5 + " Counting Sort Method (Integer Sorting Algorithm) " + "#" * 5)
+
+arr: list[int] = [10, 7, 1, 8, 2, 5, 3, 11, 4, 0]
+
+print(f"List before sorting it -> {arr}")
+
+sorter.counting_sorting(arr)
+
+print(f"List after sorting it -> {arr}")
+
+print("#" * 5 + " Testing Sorting Algorithms on large list of integers (N=1,000,000) " + "#" * 5)
+
+size: int = 1000000
+max_value: int = 100000
+
+arr: list[int] = rand_int_list_generator(max_value, size)
+
+start = timer()
+print(f"sorter.quick_sort(arr)) : {sorter.quick_sort(arr)}")
+end = timer()
+print(f"It took {end-start} seconds")
+# print(arr)
+
+# WARNING: merge sort implementation in python takes forever on large lists
+
+# arr: list[int] = rand_int_list_generator(max_value, size)
+
+# start = timer()
+# print(f"sorter.merge_sort(arr)) : {sorter.merge_sort(arr)}")
+# end = timer()
+# print(f"It took {end-start} seconds")
+
+arr: list[int] = rand_int_list_generator(max_value, size)
+
+start = timer()
+print(f"sorter.counting_sort(arr)) : {sorter.counting_sorting(arr)}")
+end = timer()
+print(f"It took {end-start} seconds")
+# print(arr)
