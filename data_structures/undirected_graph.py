5b7d25b77e49a9cbdf1cd260d1f898a8d323b50a
rock-and-code <105887521+rock-and-code@users.noreply.github.com>
2023-09-19 17:25:41 -0400

    Adding spanning_tree() method to undirected_graph.py file

--- data_structures/undirected_graph.py
+++ data_structures/undirected_graph.py
@@ -1,9 +1,12 @@
 from stack import Stack
 from myqueue import Queue
+import copy
 
 from typing import TypeVar, Generic
 
 T = TypeVar("T")
+
+Self = TypeVar("Self", bound="UndirectedGraph")
 
 class UndirectedGraph(Generic[T]):
     """
@@ -22,18 +25,43 @@ class UndirectedGraph(Generic[T]):
             self.destination: T = destination
             self.weight: int = weight
 
+
+        def __str__(self) -> str:
+            """
+            Returns a string representation of the graph edge
+            """
+            return f"Source: {self.source} Destination: {self.destination} Weight: {self.weight}"
+        
+        def __eq__(self, obj: object) -> bool:
+            """
+            Returns true if the given edge is equals to this edge
+            """
+            if isinstance(obj, UndirectedGraph.Edge):
+                return self.source == obj.source and self.destination == obj.destination
+            return NotImplemented
+        
+        def __ne__(self, other):
+            x = self.__eq__(other)
+            if x == NotImplemented:
+                return NotImplemented
+            return not x
+        
+        def __hash__(self):
+            return hash(self.source + self.destination)
+
     def __init__(self) -> None:
         super().__init__()
         self.adj_list: dict[T, set[UndirectedGraph.Edge]] = {}
         self.vertexes: int = 0
 
+    
     def add_vertex(self, vertex: T) -> None:
         """
         Inserts a new vertex in this graph if it does not exists.
         """
         if vertex not in self.adj_list:
             self.vertexes += 1
-        self.adj_list[vertex] = set()
+            self.adj_list[vertex] = set()
 
     def add_edge(self, source: T, destination: T, weight: int) -> None:
         """
@@ -42,7 +70,7 @@ class UndirectedGraph(Generic[T]):
         """
         if source in self.adj_list and destination in self.adj_list:
             self.adj_list[source].add(UndirectedGraph.Edge(source, destination, weight))
-            self.adj_list[source].add(UndirectedGraph.Edge(source, destination, weight))
+            self.adj_list[destination].add(UndirectedGraph.Edge(destination, source, weight))
 
     def remove_edge(self, source: T, destination: T, weight: int) -> None:
         """
@@ -50,10 +78,10 @@ class UndirectedGraph(Generic[T]):
         in the graph
         """
         if source in self.adj_list and destination in self.adj_list:
-            for edge in self.adj_list[source]:
+            for edge in self.adj_list[source].copy():
                 if edge.source == source and edge.destination == destination:
                     self.adj_list[source].remove(edge)
-            for edge in self.adj_list[destination]:
+            for edge in self.adj_list[destination].copy():
                 if edge.source == destination and edge.destination == source:
                     self.adj_list[destination].remove(edge)
 
@@ -64,20 +92,23 @@ class UndirectedGraph(Generic[T]):
         visited: set[T] = set()
         for k in self.adj_list:
             if k not in visited:
-                return self.is_cyclic_util(k, visited, None)
+                if self.is_cyclic_util(k, visited, None):
+                    return True
         return False
     
     def is_cyclic_util(self, vertex: T, visited: set[T], parent: T) -> bool:
         visited.add(vertex)
         for edge in self.adj_list[vertex]:
             if edge.destination not in visited:
-                if self.is_cyclic_util(edge.destination, visited, parent):
+                if self.is_cyclic_util(edge.destination, visited, vertex):
                     return True
-                elif edge.destination != vertex:
+            elif edge.destination != parent:
                     return True
         return False
     
     def bfs(self, source: T) -> None:
+        if source not in self.adj_list:
+            return
         """
         Traverses and prints the vertexes of this graph using a breath search first
         algorith from a given source vertex
@@ -96,7 +127,9 @@ class UndirectedGraph(Generic[T]):
                     queue.add(edge.destination)
         print("]")
 
-    def dfs(self, source: T) -> None:
+    def dfs(self, source: T) -> Self:
+        if source not in self.adj_list:
+            return
         """
         Traverses and prints the vertexes of this graph using a depth first search
         algorith from a given source vertex
@@ -117,6 +150,10 @@ class UndirectedGraph(Generic[T]):
 
 
     def shortes_path(self, source: T) -> None:
+        """
+        Traverses the graph and prints the vertices with its shortest distance from 
+        a given source
+        """
         visited: set[T] = set()
         visited.add(source)
         distance: dict[T, int] = {}
@@ -140,6 +177,59 @@ class UndirectedGraph(Generic[T]):
             print(f"{vertex} : {distance[vertex]}")
 
 
-        # TODO -> IMPLEMENT  KRUSKA'S SPANNING TREE ALGORITHM
+        # Time: O(MN + N LOG N)
+    def spanning_tree(self):
+        """
+        Returns a new unidirected graph with all the vertexes of the 
+        of this graph and the minimum number of edges
+        """
+        # Getting all the egdes from the graph
+        visited: set[T] = set()
+        spanning_tree: UndirectedGraph = UndirectedGraph()
+        edges: list[UndirectedGraph.Edge] = []
+
+        for vertex in self.adj_list:
+            visited.add(vertex)
+            spanning_tree.add_vertex(vertex)
+            for edge in self.adj_list[vertex]:
+                if edge.destination not in visited:
+                    edges.append(edge)
+                    # visited.add(edge.destination)
+
+        edges.sort(key=lambda edge: edge.weight)
+        added_edges: int = 0 
+        for edge in edges:
+            spanning_tree.add_edge(edge.source, edge.destination, edge.weight)
+            added_edges += 1
+            if spanning_tree.is_cyclic():
+                added_edges -= 1
+                spanning_tree.remove_edge(edge.source, edge.destination, edge.weight)
+            if added_edges == len(self.adj_list) - 1:
+                break
+
+        return spanning_tree
+    
+    def print_edges(self) -> None:
+        total_cost: int = 0
+        visited: set[T] = set()
+        print(f"Edges: ")
+        for vertex in self.adj_list:
+            visited.add(vertex)
+            for edge in self.adj_list[vertex]:
+                if edge.destination not in visited:
+                    # visited.add(edge.destination)
+                    total_cost += edge.weight
+                    print(edge)
+        print(f"Total cost: {total_cost}")
+
+    def print_vertexes(self) -> None:
+        print("Vertexes [", end=" ")
+        for vertex in self.adj_list:
+            print(vertex, end=" ")
+        print("]")
+
+    
+
+
 
     
--- data_structures/undirected_graph_test.py
+++ data_structures/undirected_graph_test.py
@@ -10,24 +10,24 @@ class UndirectedGraphTest(unittest.TestCase):
     def test_bsf_method(self) -> None:
         print("#" * 5 + " Testing BFS Method " + 5 * "#")
 
-        graph.add_vertex("A");
-        graph.add_vertex("B");
-        graph.add_vertex("C");
-        graph.add_vertex("D");
-        graph.add_vertex("E");
-        graph.add_vertex("F");
-        graph.add_vertex("G");
+        graph.add_vertex("A")
+        graph.add_vertex("B")
+        graph.add_vertex("C")
+        graph.add_vertex("D")
+        graph.add_vertex("E")
+        graph.add_vertex("F")
+        graph.add_vertex("G")
 
-        graph.add_edge("A", "B", 2);
-        graph.add_edge("A", "C", 1);
-        graph.add_edge("B", "C", 3);
-        graph.add_edge("B", "G", 1);
-        graph.add_edge("B", "D", 6);
-        graph.add_edge("C", "G", 4);
-        graph.add_edge("C", "E", 5);
-        graph.add_edge("D", "E", 5);
-        graph.add_edge("D", "F", 1);
-        graph.add_edge("E", "F", 2);
+        graph.add_edge("A", "B", 2)
+        graph.add_edge("A", "C", 1)
+        graph.add_edge("B", "C", 3)
+        graph.add_edge("B", "G", 1)
+        graph.add_edge("B", "D", 6)
+        graph.add_edge("C", "G", 4)
+        graph.add_edge("C", "E", 5)
+        graph.add_edge("D", "E", 5)
+        graph.add_edge("D", "F", 1)
+        graph.add_edge("E", "F", 2)
 
         graph.bfs("A")
 
@@ -36,24 +36,24 @@ class UndirectedGraphTest(unittest.TestCase):
     def test_dfs_method(self) -> None:
         print("#" * 5 + " Testing DFS Method " + 5 * "#")
 
-        graph.add_vertex("A");
-        graph.add_vertex("B");
-        graph.add_vertex("C");
-        graph.add_vertex("D");
-        graph.add_vertex("E");
-        graph.add_vertex("F");
-        graph.add_vertex("G");
+        graph.add_vertex("A")
+        graph.add_vertex("B")
+        graph.add_vertex("C")
+        graph.add_vertex("D")
+        graph.add_vertex("E")
+        graph.add_vertex("F")
+        graph.add_vertex("G")
 
-        graph.add_edge("A", "B", 2);
-        graph.add_edge("A", "C", 1);
-        graph.add_edge("B", "C", 3);
-        graph.add_edge("B", "G", 1);
-        graph.add_edge("B", "D", 6);
-        graph.add_edge("C", "G", 4);
-        graph.add_edge("C", "E", 5);
-        graph.add_edge("D", "E", 5);
-        graph.add_edge("D", "F", 1);
-        graph.add_edge("E", "F", 2);
+        graph.add_edge("A", "B", 2)
+        graph.add_edge("A", "C", 1)
+        graph.add_edge("B", "C", 3)
+        graph.add_edge("B", "G", 1)
+        graph.add_edge("B", "D", 6)
+        graph.add_edge("C", "G", 4)
+        graph.add_edge("C", "E", 5)
+        graph.add_edge("D", "E", 5)
+        graph.add_edge("D", "F", 1)
+        graph.add_edge("E", "F", 2)
 
         graph.dfs("A")
 
@@ -62,28 +62,89 @@ class UndirectedGraphTest(unittest.TestCase):
     def test_shortest_path_method(self) -> None:
         print("#" * 5 + " Testing Shortest Path Method " + 5 * "#")
 
-        graph.add_vertex("A");
-        graph.add_vertex("B");
-        graph.add_vertex("C");
-        graph.add_vertex("D");
-        graph.add_vertex("E");
-        graph.add_vertex("F");
-        graph.add_vertex("G");
+        graph.add_vertex("A")
+        graph.add_vertex("B")
+        graph.add_vertex("C")
+        graph.add_vertex("D")
+        graph.add_vertex("E")
+        graph.add_vertex("F")
+        graph.add_vertex("G")
 
-        graph.add_edge("A", "B", 2);
-        graph.add_edge("A", "C", 1);
-        graph.add_edge("B", "C", 3);
-        graph.add_edge("B", "G", 1);
-        graph.add_edge("B", "D", 6);
-        graph.add_edge("C", "G", 4);
-        graph.add_edge("C", "E", 5);
-        graph.add_edge("D", "E", 5);
-        graph.add_edge("D", "F", 1);
-        graph.add_edge("E", "F", 2);
+        graph.add_edge("A", "B", 2)
+        graph.add_edge("A", "C", 1)
+        graph.add_edge("B", "C", 3)
+        graph.add_edge("B", "G", 1)
+        graph.add_edge("B", "D", 6)
+        graph.add_edge("C", "G", 4)
+        graph.add_edge("C", "E", 5)
+        graph.add_edge("D", "E", 5)
+        graph.add_edge("D", "F", 1)
+        graph.add_edge("E", "F", 2)
 
         graph.shortes_path("A")
 
         self.assertTrue(graph.is_cyclic())
+
+    def test_remove_edge(self) -> None:
+        print("#" * 5 + " Testing Remove Ege Method " + 5 * "#")
+
+        graph.add_vertex("A")
+        graph.add_vertex("B")
+        graph.add_vertex("C")
+        graph.add_vertex("D")
+        graph.add_vertex("E")
+        graph.add_vertex("F")
+        graph.add_vertex("G")
+
+        graph.add_edge("A", "B", 2)
+        graph.add_edge("A", "C", 1)
+        graph.add_edge("B", "C", 3)
+        graph.add_edge("B", "G", 1)
+        graph.add_edge("B", "D", 6)
+        graph.add_edge("C", "G", 4)
+        graph.add_edge("C", "E", 5)
+        graph.add_edge("D", "E", 5)
+        graph.add_edge("D", "F", 1)
+        graph.add_edge("E", "F", 2)
+
+        # graph.print_all_edges()
+
+        graph.remove_edge("E", "F", 2)
+
+        # graph.print_all_edges()
+
+    def test_spanning_tree_method(self) -> None:
+        print("#" * 5 + " Testing Spanning Tree Method " + 5 * "#")
+
+        graph.add_vertex("A")
+        graph.add_vertex("B")
+        graph.add_vertex("C")
+        graph.add_vertex("D")
+        graph.add_vertex("E")
+        graph.add_vertex("F")
+        graph.add_vertex("G")
+
+        graph.add_edge("A", "B", 2)
+        graph.add_edge("A", "C", 1)
+        graph.add_edge("B", "C", 3)
+        graph.add_edge("B", "G", 1)
+        graph.add_edge("B", "D", 6)
+        graph.add_edge("C", "G", 4)
+        graph.add_edge("C", "E", 5)
+        graph.add_edge("D", "E", 5)
+        graph.add_edge("D", "F", 1)
+        graph.add_edge("E", "F", 2)
+
+        # graph.print_edges()
+
+        spanning_tree: UndirectedGraph = graph.spanning_tree()
+
+        spanning_tree.print_edges()
+
+        print(f"Is spanning tree cyclic: {spanning_tree.is_cyclic()}")
+
+        self.assertFalse(spanning_tree.is_cyclic())
+
 
 if __name__ == '__main__':
     unittest.main()
